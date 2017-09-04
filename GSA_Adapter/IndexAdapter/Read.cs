9f9d0becbb6c4565049702698137e486a155d6ce
Arnaud Declercq <arnaud.declercq@burohappold.com>
2017-09-04 10:40:54 +0100

    moving between computers

---
+++ GSA_Adapter/IndexAdapter/Read.cs
@@ -0,0 +1,38 @@
+﻿using BH.oM.Base;
+using BH.oM.Materials;
+using BH.oM.Structural.Elements;
+using BH.oM.Structural.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.GSA
+{
+    public partial class GSAAdapter
+    {
+        /***************************************************/
+        /**** Index Adapter Methods                     ****/
+        /***************************************************/
+
+        protected override IEnumerable<BHoMObject> Read(Type type, List<string> indices)
+        {
+            // Define the dictionary of Pull methods
+            Dictionary<Type, Func<List<string>, IList>> m_PullMethods = new Dictionary<Type, Func<List<string>, IList>>()
+            {
+                {typeof(Material), PullMaterials },
+                {typeof(SectionProperty), PullSectionProperties },
+                {typeof(Node), PullNodes },
+                {typeof(Bar), PullBars }
+            };
+
+            // Get the objects based on the indices
+            if (m_PullMethods.ContainsKey(type))
+                return m_PullMethods[type](indices).Cast<BHoMObject>();
+            else
+                return new List<BHoMObject>();
+        }
+    }
+}
