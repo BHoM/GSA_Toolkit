81828c0e7c2a5cba59deaf25e7729a82a03d4141
INaslund <isak.naslund@burohappold.com>
2018-01-26 17:23:40 +0100

    Create methods for MeshFace and RigidLink

---
+++ GSA_Engine/Convert/Private Helpers/MeshFace.cs
@@ -0,0 +1,13 @@
+﻿using BH.Engine.Structure;
+using BH.oM.Base;
+using BH.oM.Structural.Elements;
+using BH.oM.Structural.Properties;
+
+namespace BH.Engine.GSA
+{
+    public static partial class Convert
+    {
+        /***************************************/
+
+    }
+}
