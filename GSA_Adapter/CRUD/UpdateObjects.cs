6f341ff2c2c62e321aff56448fdeb3beacec2cf4
INaslund <isak.naslund@burohappold.com>
2018-02-26 17:29:47 +0100

    Implementing UpdateObejcts and link bugfix

---
+++ GSA_Adapter/CRUD/UpdateObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+
+namespace BH.Adapter.GSA
+{
+    public partial class GSAAdapter
+    {
+        /***************************************************/
+        /**** Adapter Methods                           ****/
+        /***************************************************/
+
+        protected override bool UpdateObjects<T>(IEnumerable<T> objects)
+        {
+            return Create(objects, false);
+        }
+
+        /***************************************************/
+    }
+}
