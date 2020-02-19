b30cc34f5845b3f7f755ce6a163af361a058cfb7
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-19 18:26:31 +0100

    Add engine method for setting fragment part to a combination

---
+++ GSA_Engine/Modify/SetAnalysisType.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Base;
+using BH.oM.Structure.Loads;
+using BH.oM.External.GSA;
+
+namespace BH.Engine.External.GSA
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Sets the analysis type and stage for a LoadCombination")]
+        [Input("loadcombination", "The load combination to set stage and analysis type for.")]
+        [Input("stage", "The stage number for the combination to be run on.")]
+        [Output("loadCombination", "The loadcombination with set analysis type and stage.")]
+        public static LoadCombination SetAnalysisType(LoadCombination loadcombination, AnalysisType analysisType = AnalysisType.LinearStatic, int stage = 0)
+        {
+            AnalysisTaskFragment fragment = new AnalysisTaskFragment { AnalysisType = analysisType, Stage = stage };
+            LoadCombination clone = loadcombination.GetShallowClone() as LoadCombination;
+
+            clone.Fragments.AddOrReplace(fragment);
+            return clone;
+        }
+
+        /***************************************************/
+    }
+}
