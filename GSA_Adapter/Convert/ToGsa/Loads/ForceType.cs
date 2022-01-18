fdb57bdf2cf7b4297ed1a91173c39df6322a5c80
Isak Naslund <Isak.Naslund@burohappold.com>
2022-01-18 17:30:05 +0100

    fix pushing of Bar temperature loads for GSA 10

--- GSA_Adapter/Convert/ToGsa/Loads/ForceType.cs
+++ GSA_Adapter/Convert/ToGsa/Loads/ForceType.cs
@@ -82,7 +82,11 @@ namespace BH.Adapter.GSA
 
         private static string ForceTypeString(this BarUniformTemperatureLoad load)
         {
+#if GSA_10_1
+            return "LOAD_1D_THERMAL.2";
+#else
             return "TEMP_BEAM";
+#endif
         }
 
         /***************************************************/
