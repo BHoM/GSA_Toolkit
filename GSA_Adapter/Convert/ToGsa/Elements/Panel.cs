df2e55ef98be943daec2747c59e3e4be6cb3911f
Isak Naslund <Isak.Naslund@burohappold.com>
2022-01-31 09:37:59 +0100

    Add support for pushing linear panels to GSA

--- GSA_Adapter/Convert/ToGsa/Elements/Panel.cs
+++ GSA_Adapter/Convert/ToGsa/Elements/Panel.cs
@@ -20,6 +20,10 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using BH.Engine.Serialiser;
 using BH.Engine.Adapter;
 using BH.oM.Adapters.GSA;
@@ -27,9 +31,9 @@ using BH.Engine.Structure;
 using BH.oM.Structure.Elements;
 using BH.oM.Structure.Constraints;
 using BH.oM.Base;
-using System;
 using BH.Engine.Adapters.GSA;
 using BH.Engine.Base;
+using BH.oM.Adapters.GSA.Fragments;
 
 namespace BH.Adapter.GSA
 {
@@ -39,15 +43,38 @@ namespace BH.Adapter.GSA
         /**** Public  Methods                           ****/
         /***************************************************/
 
-        private static string ToGsaString(this Panel bar, string index)
+#if GSA_10_1
+
+        private static string ToGsaString(this Panel panel, string index)
         {
 
+            PanelBoundaryNodeFragment fragment = panel.FindFragment<PanelBoundaryNodeFragment>();
+
+            if (fragment == null)
+                return "";
+
+            string topo = fragment.ExternalNodes.Select(x => x.GSAId().ToString()).Aggregate((a, b) => a + " " + b);
+
+            foreach (List<Node> openingNodes in fragment.OpeningNodes)
+            {
+                topo += " V(" + openingNodes.Select(x => x.GSAId().ToString()).Aggregate((a, b) => a + " " + b) + ")";
+            }
+
             //MEMB.8	1		NO_RGB	2D_GENERIC	ALL	1	1	1 2 3 4 V(5 6 7 8)	0	35.8	0	YES	LINEAR	0	0	0	0	0	0	ACTIVE	0	NO	REBAR_2D.1	0.03	0.03	0
-            return "";
+            return $"MEMB.8, {index}, {panel.Name}, NO_RGB, 2D_GENERIC, ALL,{panel.Property.GSAId()}, 1, {topo}, 0, {panel.OrientationAngle * 180 / Math.PI}, 0, YES,LINEAR,0,0,0,0,0,0,ACTIVE,0,NO,REBAR_2D.1,0.03,0.03,0";
         }
 
         /***************************************************/
-        
+#else
+
+        public static string ToGsaString(this Panel obj, string index)
+        {
+            Engine.Base.Compute.RecordWarning("GSA has no meshing capabilities and does therefore not support Panel objects. \n"+
+                                                    "To be able to push a Panel it first needs to be meshed and turned into a FEMesh.");
+            return "";
+        }
+#endif
+        /***************************************/
     }
 }
 
