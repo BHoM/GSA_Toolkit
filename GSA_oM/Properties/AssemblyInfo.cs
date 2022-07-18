c41e2280599851fd98933b9e15980c0f811d2fa7
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:03 +0100

    Update Assembly Information

--- GSA_oM/Properties/AssemblyInfo.cs
+++ GSA_oM/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GSA_oM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/GSA_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GSA_oM")]
