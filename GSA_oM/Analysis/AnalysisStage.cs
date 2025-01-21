97c395e9d4102b71eec70ed59222049a3728cd38
Christopher Short <Christopher.Short@burohappold.com>
2025-01-21 16:44:51 +0000

    Update AnalysisStage.cs

--- GSA_oM/Analysis/AnalysisStage.cs
+++ GSA_oM/Analysis/AnalysisStage.cs
@@ -38,6 +38,8 @@ namespace BH.oM.Adapters.GSA.Analysis
 
         /***************************************************/
 
+        //Text line
+
         [Description("GSA Id. This is an int. Must be greater than 0.")]
         public virtual int Number { get; set; }
 
